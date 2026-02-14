9a5fd203f096bacbea3ebe12e9814ff3a0f7fe4b
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2026-02-13 23:44:39 -0300

    style: muda icone endereco

--- components/pagina-contato.tsx
+++ components/pagina-contato.tsx
@@ -122,15 +122,11 @@ export default function PaginaContato() {
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
                       height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
+                      fill="currentColor"
+                      className="bi bi-geo-alt-fill"
+                      viewBox="0 0 16 16"
                     >
-                      <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-                      <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+                      <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
                     </svg>
                   </div>
                   <div>
