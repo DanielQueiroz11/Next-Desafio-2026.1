d3310323b2adc8b1ad44db393e1c484efb8c6a70
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2026-02-15 23:23:27 -0300

    style: remove espacamento extra do email

--- components/footer.tsx
+++ components/footer.tsx
@@ -87,7 +87,7 @@ export default function Footer() {
               </svg>
               <span>(32) 93988-7592</span>
             </div>
-            <div className="flex items-center justify-start gap-2 mt-2">
+            <div className="flex items-center justify-start gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                 <polyline points="22,6 12,13 2,6"></polyline>
