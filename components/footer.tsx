978de7a86c322b5a2dbc4d35353c9dcdcde582b7
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2026-02-15 23:02:12 -0300

    feat: separa copyright PC e mobile e altera alguns paddings

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,85 +6,8 @@ export default function Footer() {
     <footer className="w-full bg-rock-dark text-white py-10 border-t border-white/10">
       <div className="max-w-7xl mx-auto px-8 grid grid-cols-1 md:grid-cols-3 gap-8 items-center text-center md:text-left">
         
-        {/* redes sociais */}
-<div className="flex flex-col items-center md:items-start gap-4">
-  <Link 
-    href="https://linktr.ee/the.rock.store" 
-    target="_blank" 
-    rel="noopener noreferrer"
-    className="group"
-  >
-    <h3 className="font-bold text-lg tracking-wider hover:text-rock-red transition-colors cursor-pointer">
-      Redes Sociais
-    </h3>
-  </Link>
-          <div className="flex gap-4">
-            <Link href="#" className="hover:text-rock-red transition-colors">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-              </svg>
-            </Link>
-            <a href="#" className="hover:text-rock-red transition-colors">
-              <span className="sr-only">WhatsApp</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="bi bi-whatsapp"
-                viewBox="0 0 16 16"
-              >
-                <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
-              </svg>
-            </a>
-            <Link href="#" className="hover:text-rock-red transition-colors">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-              </svg>
-            </Link>
-            <Link href="#" className="hover:text-rock-red transition-colors">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="bi bi-tiktok"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z" />
-              </svg>
-            </Link>
-          </div>
-          <div className="text-xs text-gray-400 pt-6 font-mono text-center md:text-left">
-            <p>© 2026 Caverna do Rock</p>
-            <p>Todos os direitos reservados.</p>
-          </div>
-        </div>
-
-        {/* logo central */}
-        <div className="flex justify-center self-start md:-mt-4">
+        {/* logo */}
+        <div className="flex justify-center md:justify-center self-start md:-mt-4 md:order-2 pb-1">
           <Link
             href="/"
             className="cursor-pointer transition-all duration-300 hover:scale-103"
@@ -99,61 +22,73 @@ export default function Footer() {
           </Link>
         </div>
 
-        {/* contato */}
-        <div className="flex flex-col items-center md:items-end gap-4 self-start">
-          <Link href="/contato" className="w-full text-center md:text-right md:pr-20">
-  <h3 className="font-bold text-lg tracking-wider hover:text-rock-red transition-colors cursor-pointer">
-    Contato
-  </h3>
-</Link>
+        {/* redes sociais */}
+        <div className="flex flex-col items-center md:items-start gap-4 md:order-1">
+          <Link 
+            href="https://linktr.ee/the.rock.store" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="group"
+          >
+            <h3 className="font-bold text-lg tracking-wider hover:text-rock-red transition-colors cursor-pointer">
+              Redes Sociais
+            </h3>
+          </Link>
+          <div className="flex gap-4">
+            <Link href="#" className="hover:text-rock-red transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+              </svg>
+            </Link>
+            <a href="#" className="hover:text-rock-red transition-colors">
+              <span className="sr-only">WhatsApp</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
+                <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+              </svg>
+            </a>
+            <Link href="#" className="hover:text-rock-red transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+              </svg>
+            </Link>
+            <Link href="#" className="hover:text-rock-red transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-tiktok" viewBox="0 0 16 16">
+                <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z" />
+              </svg>
+            </Link>
+          </div>
+          
+          {/* copyright PC*/}
+          <div className="hidden md:block text-xs text-gray-400 pt-6 font-mono text-left">
+            <p>© 2026 Caverna do Rock</p>
+            <p>Todos os direitos reservados.</p>
+          </div>
+        </div>
 
+        {/* contato */}
+        <div className="flex flex-col items-center md:items-end gap-4 self-start md:order-3 pt-5">
+          <Link href="/contato" className="w-full text-center md:text-right md:pr-20">
+            <h3 className="font-bold text-lg tracking-wider hover:text-rock-red transition-colors cursor-pointer">
+              Contato
+            </h3>
+          </Link>
           <div className="flex flex-col gap-2 text-sm text-left">
             <div className="flex items-center justify-start gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
               <span>(32) 98893-2957</span>
             </div>
-
             <div className="flex items-center justify-start gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
               <span>(32) 3428-6098</span>
             </div>
-
             <div className="flex items-center justify-start gap-2 mt-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                 <polyline points="22,6 12,13 2,6"></polyline>
               </svg>
@@ -161,7 +96,17 @@ export default function Footer() {
             </div>
           </div>
         </div>
+
       </div>
+
+      {/* copyright mobile */}
+      <div className="md:hidden max-w-7xl mx-auto px-8 mt-8 pt-7">
+        <div className="text-xs text-gray-400 font-mono text-center">
+          <p>© 2026 Caverna do Rock</p>
+          <p>Todos os direitos reservados.</p>
+        </div>
+      </div>
+      
     </footer>
   );
 }
