9e53696ee9b8b9ebf572327cb6508fb45fbe3f74
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2026-02-15 23:19:11 -0300

    chore: atualiza numero de telefone

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-8 grid grid-cols-1 md:grid-cols-3 gap-8 items-center text-center md:text-left">
         
         {/* logo */}
-        <div className="flex justify-center md:justify-center self-start md:-mt-4 md:order-2 pb-1">
+        <div className="flex justify-center md:justify-center self-start md:-mt-4 md:order-2 pb-2">
           <Link
             href="/"
             className="cursor-pointer transition-all duration-300 hover:scale-103"
@@ -85,7 +85,7 @@ export default function Footer() {
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
-              <span>(32) 3428-6098</span>
+              <span>(32) 93988-7592</span>
             </div>
             <div className="flex items-center justify-start gap-2 mt-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
