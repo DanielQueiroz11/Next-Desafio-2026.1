904c2260e474bd22d53cff4a846673bebe85c8a1
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2026-02-18 15:02:56 -0300

    style: modifica parametros funcao

--- src/app/produtos-individuais/[id]/page.tsx
+++ src/app/produtos-individuais/[id]/page.tsx
@@ -1,6 +1,6 @@
 import PaginaVisualizacao from "@/components/pagina-visualizacao/pagina-visualizacao";
 
-export default function ProdutoDinamicoPage({ params }: { params: { id: string } }) {
-
+// para futuramente: export default function ProdutoDinamicoPage({ params }: { params: { id: string } }) {
+export default function ProdutoDinamicoPage() {
   return <PaginaVisualizacao />;
 }
