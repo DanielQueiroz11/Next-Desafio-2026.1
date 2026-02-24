c25e74195e48a3e361f7e347c19b3b29299faee6
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2026-02-24 14:42:14 -0300

    fix: corrige erro que fazia tocar o radio em mobile e tablet ao clicar na palavra som

--- components/pagina-inicial/hero-section.tsx
+++ components/pagina-inicial/hero-section.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { useAudio } from "../../src/providers/audio-context";
 
 export default function HeroSection() {
-  const { toggleAudio } = useAudio();
-
   return (
     <section className="relative w-full min-h-[450px] flex flex-col items-center justify-center text-center overflow-hidden bg-rock-dark">
       {/* imagem de fundo */}
@@ -23,10 +20,7 @@ export default function HeroSection() {
       <div className="relative z-10 px-4 max-w-[1300px] mx-auto w-full">
         <h1 className="text-[35px] md:text-[42px] lg:text-[52px] xl:text-[56px] 2xl:text-[62px] font-black mb-6 tracking-normal text-white drop-shadow-lg uppercase transition-all duration-300">
           Vista o{" "}
-          <span
-            onClick={toggleAudio}
-            className="text-rock-red cursor-pointer hover:brightness-125 transition-all"
-          >
+          <span className="text-rock-red">
             som
           </span>{" "}
           que você <span className="text-rock-red">ama</span>
