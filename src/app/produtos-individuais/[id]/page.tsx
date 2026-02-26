5303ec19abb32e2c636c519cd5292c52adc1f1d4
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2026-02-26 16:41:30 -0300

    feat: integra pagina de vi com prisma

--- src/app/produtos-individuais/[id]/page.tsx
+++ src/app/produtos-individuais/[id]/page.tsx
@@ -1,11 +1,11 @@
 import db from "@/src/lib/db";
 import PaginaVisualizacao from "@/components/pagina-visualizacao/pagina-visualizacao";
 
-export default async function ProdutoPage({
-  params,
-}: {
-  params: { id: string };
+export default async function ProdutoPage(props: {
+  params: Promise<{ id: string }>;
 }) {
+  const params = await props.params; 
+
   const produtoDoBanco = await db.product.findUnique({
     where: {
       id: Number(params.id), 
@@ -14,7 +14,7 @@ export default async function ProdutoPage({
 
   if (!produtoDoBanco) {
     return (
-      <div className="flex h-screen items-center justify-center bg-rock-dark text-white text-2xl">
+      <div className="flex h-screen items-center justify-center bg-rock-dark text-white text-2xl font-bold">
         Produto não encontrado na Caverna do Rock 🤘
       </div>
     );
